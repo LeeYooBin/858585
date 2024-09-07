fa4654975730c34a18cebb24826216b9e3e6f1b9
LeeYooBin <santosroberdan25@gmail.com>
2024-09-07 18:07:05 -0300

    feat: creating and styling app component

--- cat-blog/src/app/app.component.ts
+++ cat-blog/src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { MainArticleComponent } from './main-article/main-article.component';
+import { RelatedArticlesComponent } from './related-articles/related-articles.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    MainArticleComponent,
+    RelatedArticlesComponent,
+    FooterComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
